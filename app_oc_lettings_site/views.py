f80a5676a7b213ee20b9487d1e0b59f7ab91f832
EnguerrandF <feignon.enguerrand@outlook.fr>
2023-10-16 16:18:28 +0200

    soutenance

--- app_oc_lettings_site/views.py
+++ app_oc_lettings_site/views.py
@@ -5,9 +5,7 @@ def index(request):
 
     """ Lorem ipsum dolor sit amet, consectetur adipiscing elit.
     Quisque molestie quam lobortis leo consectetur ullamcorper non id est.
-    Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros, vitae
-    dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum,
-    eget consequat ipsum lobortis quis.
+    Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros, vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis.
     Phasellus eleifend ex auctor venenatis tempus.
     Aliquam vitae erat ac orci placerat luctus. Nullam elementum urna nisi,
     pellentesque iaculis enim cursus in. Praesent volutpat porttitor magna,
